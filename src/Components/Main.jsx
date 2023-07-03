1a34782c0727406a846a7f72a4e1989b7eb92eed
tal krugman <talkrugman@gmail.com>
2023-07-04 00:34:57 +0300

    new

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,6 +1,27 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../firebase/app';
+
+const auth = getAuth();
+
+function test() {
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+        });
+}
+
 function Main() {
     return <main>
-        <p>Arbell Krugman!!!!</p>
+        <button onClick={test}>
+            register
+        </button>
     </main>
 }
 
---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCq2mLM1awQ3isiJnYtIkKIuxu2-uLnVSg",
+    authDomain: "project-a-62a5f.firebaseapp.com",
+    projectId: "project-a-62a5f",
+    storageBucket: "project-a-62a5f.appspot.com",
+    messagingSenderId: "877308824333",
+    appId: "1:877308824333:web:ffa546f657735aaf26be44",
+    measurementId: "G-SCEV7NWL8P"
+  };
+
+  export default firebaseConfig;
+  
