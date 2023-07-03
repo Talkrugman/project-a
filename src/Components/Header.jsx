0412c486a4b8cbfe5a34bf5d6da6b791eb2ee60f
tal krugman <talkrugman@gmail.com>
2023-07-03 11:53:47 +0300

    add react app

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return <header>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    </header>
+}
+
+export default Header
---
+++ src/Components/Main.jsx
@@ -0,0 +1,7 @@
+function Main() {
+    return <main>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus illo quasi sunt? Fugiat, optio amet, eum officiis dolores odit id enim tempora modi ratione obcaecati quisquam porro alias ut consequuntur?</p>
+    </main>
+}
+
+export default Main;
